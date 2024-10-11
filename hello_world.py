fea83b58d97e21beadfde67e24a86f7ef829c875
Gerson AGC <ger.agc@gmail.com>
2024-10-11 17:13:56 +0000

    Initial commit - Hello World project

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello World")
+
+def run(server_class=HTTPServer, handler_class=HelloHandler):
+    server_address = ("", 8080)
+    httpd = server_class(server_address, handler_class)
+    print("Starting httpd server on port 8080...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
+
